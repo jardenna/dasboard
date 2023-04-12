ea13bad00d60ab74691f8bf7f2db7c44afca066d
jardenna <hbjornum@gmail.com>
2023-04-12 14:54:12 +0200

    init auth

---
+++ auth/src/bootstrap.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
+
+// Mount function to start the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+  // if we have a defaultHistory use that otherwise use createMemoryHistory
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDom.render(<App history={history} />, el);
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+
+      //We need this check to prevent race conditions
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// run mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+  if (devRoot) {
+    // if Isolation we should use browserRouter
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// if through container export the mount function, so the container can deside when to mount the function
+export { mount };
---
+++ auth/src/index.js
@@ -0,0 +1,2 @@
+// To load async
+import('./bootstrap');
