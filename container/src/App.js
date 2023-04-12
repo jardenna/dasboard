5224590d97a73e952721c9d028c53a3ff33cc5af
jardenna <hbjornum@gmail.com>
2023-04-12 22:33:51 +0200

    lazy load

--- container/src/App.js
+++ container/src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/AuthApp';
 import Header from './components/Header';
 
+// Instead of load all components on every render, we need to only load the component we need with lazy load and React Suspense component
+const lazyLoadAuthComponent = lazy(() => import('./components/AuthApp'));
+const lazyLoadMarketingComponent = lazy(() =>
+  import('./components/MarketingApp')
+);
 const App = () => {
   return (
     <BrowserRouter>
       <div>
         <Header />
-        <Switch>
-          <Route path='/auth' component={AuthApp} />
-          <Route path='/' component={MarketingApp} />
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path='/auth' component={lazyLoadAuthComponent} />
+            <Route path='/' component={lazyLoadMarketingComponent} />
+          </Switch>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
