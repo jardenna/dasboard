7e2ac36bfde56c85bf86b543a812e1e91c1e6d7d
jardenna <hbjornum@gmail.com>
2023-04-12 17:24:29 +0200

    Auth to container

---
+++ container/src/components/AuthApp.js
@@ -0,0 +1,28 @@
+import React, { useRef, useEffect } from 'react';
+import { mount } from 'auth/AuthApp';
+import { useHistory } from 'react-router-dom';
+
+const MarketingApp = () => {
+  const ref = useRef(null);
+  const history = useHistory();
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: (
+        //We need to  Destructure and rename history.pathname so we can make the check
+        { pathname: nextPathname }
+      ) => {
+        const { pathname } = history.location;
+
+        //We need this check to prevent race conditions
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    history.listen(onParentNavigate);
+  }, []);
+
+  return <div ref={ref} />;
+};
+
+export default MarketingApp;
