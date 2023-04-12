74fab78e7966a531301347db2eff7dd99965b558
jardenna <hbjornum@gmail.com>
2023-04-12 14:08:59 +0200

    navigation

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,10 +1,25 @@
 import React, { useRef, useEffect } from 'react';
 import { mount } from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: (
+        //We need to  Destructure and rename history.pathname so we can make the check
+        { pathname: nextPathname }
+      ) => {
+        const { pathname } = history.location;
+
+        //We need this check to prevent race conditions
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
 
 // Mount function to start the app
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
 
-// if Develop mode and isolation
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDom.render(<App history={history} />, el);
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
 
-const mount = (el) => {
-  ReactDom.render(<App />, el);
+      //We need this check to prevent race conditions
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 // run mount immediately
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
