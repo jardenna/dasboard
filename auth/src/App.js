39b2d580be691431aad674c2bc5ea8a784aa4c6e
jardenna <hbjornum@gmail.com>
2023-04-13 15:37:41 +0200

    Removed UI from container

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import { Switch, Route, Router } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
 
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 
-export default ({ history, onSignIn }) => {
+const App = ({ history, onSignIn }) => {
   return (
-    <div>
-      <StylesProvider>
-        <Router history={history}>
-          <Switch>
-            <Route path='/auth/signin'>
-              <Signin onSignIn={onSignIn} />
-            </Route>
-            <Route path='/auth/signup' component={Signup}>
-              <Signup onSignIn={onSignIn} />
-            </Route>
-          </Switch>
-        </Router>
-      </StylesProvider>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route path='/auth/signin'>
+          <Signin onSignIn={onSignIn} />
+        </Route>
+        <Route path='/auth/signup' component={Signup}>
+          <Signup onSignIn={onSignIn} />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
+
+export default App;
